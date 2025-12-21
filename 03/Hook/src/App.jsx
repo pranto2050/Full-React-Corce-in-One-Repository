832336ac73d9ff65cd8e25d5a1c7f27e411b2aea
pranto-islam <prantoislamnt51@gmail.com>
2025-12-21 23:52:46 +0600

    fix: update increment function to correctly set count state and improve button layout

--- 03/Hook/src/App.jsx
+++ 03/Hook/src/App.jsx
@@ -1,19 +1,31 @@
 import { useState } from 'react'
 import 'remixicon/fonts/remixicon.css'
 import './App.css'
+import { useFormState } from 'react-dom';
 
 function App() {
 
-    function my(){
-      console.log('Click Is Working');
+ 
+
+    const[count,setcount] = useState(0);
+    
+    function increment(){
+      setcount = 98;
     }
 
 
-  return (
-    <div className='p-10'>
-      <h1> React Hook Larning </h1>
 
-    <button className=' mt-10 p-5 rounded-full px-10 text-green-500 bg-amber-700' onClick={my}> Click Me <i class="ri-cursor-line"></i> </button>
+  return (
+    <div className='p-10 flex flex-col justify-center items-center'>
+      <div className=' bg-blue-700 w-90 h-50 flex justify-center items-center rounded-2xl'>
+        <h1 className=' font-bold text-8xl'> {count} </h1>
+      </div>
+
+    <div className=' flex gap-5'>
+      <button className=' mt-10 text-2xl rounded-full px-10 text-green-500 bg-amber-300 border-2' onClick={increment}> Increment </button>
+      <button className=' mt-10 text-2xl rounded-full px-10 text-green-500 bg-amber-300 border-2' > Decrement </button>
+
+    </div>
 
 
 
